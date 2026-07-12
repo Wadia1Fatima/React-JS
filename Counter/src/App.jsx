203cacb3902b8e8c5c1832f1431363382f9f7679
Wadia Fatima <wadiafatima166@gmail.com>
2026-07-12 19:56:46 +0000

    Counter Project

---
+++ Counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0);
+  // let counter = 0;
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+
+  const decreaseValue = () => {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}>Increase Counter</button>
+      <br />
+      <button
+      onClick={decreaseValue}>Decrease Counter</button>
+    </>
+  )
+}
+
+export default App
