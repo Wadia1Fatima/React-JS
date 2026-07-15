c5c8f8aac88df7f7f2174c36858e7ae671fd954c
Wadia Fatima <wadiafatima166@gmail.com>
2026-07-15 17:26:27 +0000

    Tailwind Props

---
+++ TailWindProps/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-950">
+      <h1 className="text-4xl font-black tracking-widest text-emerald-400 uppercase drop-shadow-[0_5px_5px_rgba(52,211,153,0.3)]">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
