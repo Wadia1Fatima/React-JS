b2f2bbd6c7740d21eb6d26dd7a4904dbd4242063
Wadia Fatima <wadiafatima166@gmail.com>
2026-07-15 17:50:59 +0000

    Tailwind

--- TailWindProps/src/App.jsx
+++ TailWindProps/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card1 from './Components/Card1'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-950">
+    <>
       <h1 className="text-4xl font-black tracking-widest text-emerald-400 uppercase drop-shadow-[0_5px_5px_rgba(52,211,153,0.3)]">
         Tailwind is Working!
       </h1>
-    </div>
+      <Card1 />
+    </>
   )
 }
 
