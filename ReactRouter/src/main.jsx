f88248fcd0f92786d4e231bba2346cb2be13a5fb
Wadia Fatima <wadiafatima166@gmail.com>
2026-07-24 15:03:26 +0000

    React Router

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Layout from "./Layout";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact"
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       }
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRouterFromElements(
+    <Route path = '/' element = {<Layout/>}>
+        <Route path = '' element = {<Home />} />
+        <Route path = 'about' element = {<About />} />
+        <Route path = 'contact' element = {<Contact />} />
+        <Route path = 'user/:userid' element = {<HUser />} />
+        <Route path = 'github' element = {<GitHub />} />
+    </Route>
+  )
 )
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
